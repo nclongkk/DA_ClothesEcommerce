19404f411ff2c81c7dd8cbc0f2ed061556fe9e33
nclongkk <nclongkk@gmail.com>
2022-06-25 14:59:32 +0700

    Test socket in prod

--- utils/socketHandler.js
+++ utils/socketHandler.js
@@ -4,6 +4,7 @@ const httpStatus = require('http-status');
 const socketHandler = (io) => {
   io.use(async (socket, next) => {
     try {
+      console.log('connect');
       const token = socket.handshake.auth.token;
       if (!token) {
         return next(new customError('Unauthorization', httpStatus.FORBIDDEN));
@@ -18,10 +19,11 @@ const socketHandler = (io) => {
       return next(error);
     }
   }).on('connection', (socket) => {
+    console.log(socket.user.id);
     socket.join(socket.user.id);
 
-    //emit connected event
-    // socket.emit('connected', { msg: 'connected' });
+    // emit connected event
+    socket.emit('connected', { msg: 'connected' });
   });
 };
 
